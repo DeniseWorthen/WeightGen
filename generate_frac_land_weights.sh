e67cba3251c6730b97beef33e45fb758d41ce8ed
denise.worthen <Denise.Worthen@noaa.gov>
2020-08-22 16:44:16 +0000

    change output directory to TTout2

--- generate_frac_land_weights.sh
+++ generate_frac_land_weights.sh
@@ -14,17 +14,18 @@
 
 # this script can be run in an interactive job; set nprocs to the number of processors the interactive job is using
 # salloc --x11=first -q debug -t 0:30:00 --nodes=1 -A marine-cpu
-# rgcmd="srun --ntasks=4"
+ rgcmd="srun --ntasks=4"
 # in a serial job
-rgcmd=""
+#rgcmd=""
 
 set -x
 
 module use -a /scratch1/NCEPDEV/nems/emc.nemspara/soft/modulefiles
 ESMF_BINDIR="/scratch1/NCEPDEV/nems/emc.nemspara/soft/esmf/8.0.1-intel18.0.5.274-impi2018.0.4-netcdf4.7.4_parallel.release/bin"
-SCRIP_DIR="/scratch2/NCEPDEV/climate/Denise.Worthen/TTout"
+SCRIP_DIR="/scratch2/NCEPDEV/climate/Denise.Worthen/TTout2"
 FIX_DIR="/scratch1/NCEPDEV/global/glopara/fix/fix_fv3_gmted2010"
-OUT_DIR="/scratch2/NCEPDEV/stmp1/Denise.Worthen/ForShan/mapped_omask"
+#OUT_DIR="/scratch2/NCEPDEV/stmp1/Denise.Worthen/ForShan/mapped_omask"
+OUT_DIR="/scratch2/NCEPDEV/climate/Denise.Worthen/TTout2"
 meth="conserve"
 
 
